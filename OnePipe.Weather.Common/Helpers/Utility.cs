bf4dabc2ed7e1a663398e598df4fa7223931586a
ikenna.ogbonna <ikenna.ogbonna@bankly.ng>
2023-01-12 15:47:35 +0100

    Utility method

---
+++ OnePipe.Weather.Common/Helpers/Utility.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace OnePipe.Weather.Common.Helpers
+{
+    public class Utility
+    {
+        public static string SerializeXML<T>(T dataToSerialize)
+        {
+            try
+            {
+                var stringwriter = new System.IO.StringWriter();
+                var serializer = new XmlSerializer(typeof(T));
+                serializer.Serialize(stringwriter, dataToSerialize);
+                return stringwriter.ToString();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
